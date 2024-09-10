d130fd77934864c84a22a41f39d07a67fc4d1290
Pratap Singh <vimlapratap25@gmail.com>
2024-09-10 23:29:12 +0530

    remove comment

--- src/App.js
+++ src/App.js
@@ -1,64 +1,3 @@
-// import React, { useState } from "react";
-
-// // const debugEl = document.getElementById('debug'),
-// // 			// Mapping of indexes to icons: start from banana in middle of initial position and then upwards
-// // 			iconMap = ["ICON 1", "ICON 2", "ICON 3"],
-// 			// Width of the icons
-			
-  
-
-
-// function App (){
-
-//   const icon_width = 170,	
-// 			      icon_height = 115.2,
-//             num_icons = 15,
-//             time_per_icon = 10,
-//             indexes = [0, 0, 0, 0, 0];
-
-
-
-// const roll = (reel, offset = 0) => {
-// 	const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons); 
-//   var style = getComputedStyle(reel);
-//   const backgroundPositionY = parseFloat(style["background-position-y"]);
-  
-
-//   reel.style.transition = `background-position ${(14 * delta) * time_per_icon}ms`;
-// reel.style.backgroundPosition = `0px ${backgroundPositionY + delta * icon_height}px`;
-
-
-// }
-
-// function rollAll() {
-// 	// debugEl.textContent = 'rolling...';
-// 	const reelsList = document.querySelectorAll('.slots > .reel');
-//   [...reelsList].map((reel, i) => {
-//     console.log(reel, i);
-//     roll(reel, i);
-//   })
-// }
-// rollAll();
-
-//     return(
-//     <div class="slots">
-//       <div class="reel"></div>
-//       <div class="reel"></div>
-//       <div class="reel"></div>
-//       <div class="reel"></div>
-//       <div class="reel"></div>
-//     </div>);
-    
-// }
-
-
-
-
-// export default App;
-
-
-
-
 import React from "react";
 
 function App () {
@@ -94,7 +33,7 @@ function App () {
         <div className="reel"></div>
       </div>
 
-      {/* Add circular button with an image below the slots */}
+     
       <button className="circular-button" onClick={rollAll}>
         
       </button>
